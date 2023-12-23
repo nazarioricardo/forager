9ba6cf22b7a750a5f4ccb34517903bddaa36df84
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-23 14:54:59 -0400

    Added dashboard route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(resource)
+    '/dashboard'
+  end
+
+  def after_sign_out_path_for(scope)
+    '/'
+  end
 end
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+  def show
+    # Add your logic here
+  end
+end

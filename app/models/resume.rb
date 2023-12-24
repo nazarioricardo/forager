4ea93920c646eabd9676f33ab54ada5f65c1c5ef
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-24 17:27:16 -0400

    Field validation

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,11 @@
 class Resume < ApplicationRecord
+  validates :title, presence: true
+  validate :pdf_attached
+  
   has_one_attached :pdf
   belongs_to :user
+
+  def pdf_attached
+    errors.add(:pdf, "must be attached") unless pdf.attached?
+  end
 end

5a97e357313172a89689fd9670365242d52caa87
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-25 11:53:15 -0400

    Added render page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'render/index'
   devise_for :users
-  # Defines the root path route ("/")
-  root 'pages#index'
+
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'dashboard#show'
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "render#index"
 end

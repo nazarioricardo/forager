ed92bd0000091a7f40812df7c27aa7e2afbced61
Ricardo Nazario <nazarioricardo@gmail.com>
2024-01-04 10:25:16 -0400

    Added redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(scope)
+    session.delete(:first_visit)
     '/'
   end
 end
--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -1,4 +1,15 @@
 class RenderController < ApplicationController
+  before_action :redirect_if_first_visit
+
   def index
+  end
+
+  private
+
+  def redirect_if_first_visit
+    if user_signed_in? && session[:first_visit].nil?
+      session[:first_visit] = true
+      redirect_to '/dashboard'
+    end
   end
 end

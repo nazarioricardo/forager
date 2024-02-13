edfcb1e61f701b1679aa9a760ab0c6c239f709e6
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 15:00:55 -0500

    modifyng letters

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
 
   # Letters
   resources :letters, only: [:new, :create, :show]
+  resources :letters do
+    get 'download', on: :member
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check

01b15ce76fa051bab83ff804103292e156894422
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 17:37:28 -0500

    Cleared Migrations

--- db/migrate/20231224202719_add_user_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToJobs < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :jobs, :user, null: false, foreign_key: true
-  end
-end
--- db/migrate/20231224212931_add_resume_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddResumeToJobs < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :jobs, :resume, null: false, foreign_key: true
-  end
-end

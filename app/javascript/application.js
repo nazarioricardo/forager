63ebaef601c1adb85258459c6d7da83e4b77a198
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 11:21:19 -0500

    Removed console.logs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 import { initSortable } from "./initSortable";
 
-console.log("hello?")
-document.addEventListener('turbo:load', () => {
-  console.log('hello')
+document.addEventListener("turbo:load", () => {
   const statusUls = document.querySelectorAll(".job-list");
-  console.log('uls', statusUls)
   if (statusUls) {
     initSortable(statusUls);
   }

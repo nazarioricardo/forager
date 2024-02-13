ecb251139cdb698512d45777b1332e20e21b05a0
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 13:06:11 -0500

    Fixed sign out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions' 
   }
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

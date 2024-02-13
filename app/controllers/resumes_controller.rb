db3c5ea61a85d6085674689b220c1748bc963e2b
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 16:59:40 -0500

    Downloading files using document_service

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -27,22 +27,10 @@ class ResumesController < ApplicationController
 
   def download
     @resume = Resume.find(params[:id])
-    google = GoogleService.new(current_user.google_token)
+    doc_service = DocumentService.new(current_user.google_token)
 
-    # Copy the file and convert it to a Google Doc
-    file_id = @resume.google_drive_file_id
-    copy = google.drive.copy_file(file_id, Google::Apis::DriveV3::File.new(mime_type: 'application/vnd.google-apps.document'))
-    doc_id = copy.id
-    
-    # Replace all instances of "Ricardo" with "George"
-    requests = [Google::Apis::DocsV1::Request.new(replace_all_text: Google::Apis::DocsV1::ReplaceAllTextRequest.new(contains_text: Google::Apis::DocsV1::SubstringMatchCriteria.new(text: 'Ricardo', match_case: true), replace_text: 'George'))]
-    google.docs.batch_update_document(doc_id, Google::Apis::DocsV1::BatchUpdateDocumentRequest.new(requests: requests))
-
-    # Export the Google Doc as a PDF
-    pdf_data = google.drive.export_file(doc_id, 'application/pdf')
-
-    # Send the PDF data as a file download
-    send_data pdf_data, filename: 'modified_resume.pdf', type: 'application/pdf'
+    pdf_data = doc_service.generate_pdf(@resume.google_drive_file_id)
+    send_data pdf_data, filename: "#{@resume.title} Resume.pdf", type: 'application/pdf'
   end
 
   private

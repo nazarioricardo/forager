2c6f3211ed891f98eda5a3ed7034bddb1b4fad10
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-13 14:35:28 -0500

    Modified application record for g drive

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,13 +1,8 @@
 class Letter < ApplicationRecord
   validates :title, presence: true
-  validate :pdf_attached
+  validates :google_drive_file_id, presence: true
 
-  has_one_attached :pdf
   belongs_to :user
   
   has_many :jobs
-
-  def pdf_attached
-    errors.add(:pdf, "must be attached") unless pdf.attached?
-  end
 end

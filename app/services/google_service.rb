50a80c3ea20610701b246c1cb9091c2f5caeb351
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-15 19:16:51 -0500

    Changed google logger level for less logs

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -8,6 +8,7 @@ class GoogleService
   SCOPE = Google::Apis::DriveV3::AUTH_DRIVE
   
   def initialize(access_token)
+    Google::Apis.logger.level = Logger::ERROR
     @drive = Google::Apis::DriveV3::DriveService.new
     @drive.authorization = access_token
     @docs = Google::Apis::DocsV1::DocsService.new

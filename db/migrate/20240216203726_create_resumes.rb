19c58c30f303d3ae4f1089cf160233b7258fb417
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-16 15:38:56 -0500

    Added documents

---
+++ db/migrate/20240216203726_create_resumes.rb
@@ -0,0 +1,12 @@
+class CreateResumes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :resumes do |t|
+      t.string :company, null: false
+      t.string :role, null: false
+      t.references :user, null: false, foreign_key: true
+      t.string :google_drive_file_id, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_203212) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_203815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "letters", force: :cascade do |t|
+    t.string "company", null: false
+    t.string "role", null: false
+    t.bigint "user_id", null: false
+    t.string "google_drive_file_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_letters_on_user_id"
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.string "company", null: false
+    t.string "role", null: false
+    t.bigint "user_id", null: false
+    t.string "google_drive_file_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_resumes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_203212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "letters", "users"
+  add_foreign_key "resumes", "users"
 end

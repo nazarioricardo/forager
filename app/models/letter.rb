7d908b7204bb221fb22cd3f08fca0434e02eadd2
Ricardo Nazario <nazarioricardo@gmail.com>
2024-02-17 12:21:26 -0500

    Implemented documentable module

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,16 +1,10 @@
 class Letter < ApplicationRecord
+  include Documentable
+
   validates :title, presence: true
   validates :google_drive_file_id, presence: true
 
   belongs_to :user
   
   has_many :jobs
-
-  def drive_url
-    "https://drive.google.com/file/d/#{self.google_drive_file_id}/view"
-  end
-
-  def docs_url
-    "https://docs.google.com/document/d/#{self.google_drive_file_id}/edit"
-  end
 end

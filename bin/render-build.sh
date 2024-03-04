3b2f7d3c801bc13815d94c4bd1ee830bed986975
Ricardo Nazario <nazarioricardo@gmail.com>
2024-03-04 16:25:41 -0400

    Added migrate command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
 # bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}

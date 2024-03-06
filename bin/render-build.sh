d2e45935d8f066f2ed9433af3d3f7c69e3f787f2
Ricardo Nazario <nazarioricardo@gmail.com>
2024-03-06 11:30:14 -0400

    New commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 # exit on error
 set -o errexit
 
-npm install
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+npm install
 # bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
